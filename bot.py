ebd0e0422533f09e34508b1ae0a275d31f310f50
NazarDatsenko <steamsupernazar@gmail.com>
2026-01-09 01:57:55 +0200

    initial commit

--- bot.py
+++ bot.py
@@ -1,14 +1,29 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
+from aiogram.enums import ChatType
+from aiogram.types import FSInputFile
 import os
 
 bot = Bot(token=os.environ["BOT_TOKEN"])
 dp = Dispatcher()
 
 
+
 @dp.message()
-async def hello(message: types.Message):
-    await message.answer("Хай! Я бот для обсоса Морозіли мол.")
+async def group_only(message: types.Message):
+    if message.chat.type not in (ChatType.GROUP, ChatType.SUPERGROUP):
+        return
+
+@dp.message(lambda m: m.chat.type in ("group", "supergroup") and m.text == "/hello")
+async def send_voice(message: types.Message):
+    voice = FSInputFile("hello.ogg")
+    await message.answer_voice(voice)
+
+@dp.message()
+async def keyword(message: types.Message):
+    if message.chat.type.name.endswith("GROUP"):
+        if "Морозіла" in message.text.lower():
+            await message.reply("Чую починається обсос мол.")
 
 
 async def main():
