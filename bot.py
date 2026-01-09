e7c703e7a61af59d215a168bb07a5cd75f1ea3d7
NazarDatsenko <steamsupernazar@gmail.com>
2026-01-09 02:10:59 +0200

    changed command for hello

--- bot.py
+++ bot.py
@@ -1,35 +1,28 @@
 import asyncio
+import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ChatType
 from aiogram.types import FSInputFile
 from aiogram.filters import Command
-import os
 
 bot = Bot(token=os.environ["BOT_TOKEN"])
 dp = Dispatcher()
 
-
-
-@dp.message()
-async def group_only(message: types.Message):
-    if message.chat.type not in (ChatType.GROUP, ChatType.SUPERGROUP):
-        return
-
-@dp.message(Command("hello"), lambda m: m.chat.type in ("group", "supergroup"))
+# Голосове по команді /hello
+@dp.message(Command("hello"), lambda m: m.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP))
 async def send_voice(message: types.Message):
     voice = FSInputFile("hello.ogg")
     await message.answer_voice(voice)
 
-@dp.message()
+# Ключове слово "морозіла"
+@dp.message(lambda m: m.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP))
 async def keyword(message: types.Message):
-    if message.chat.type.name.endswith("GROUP"):
-        if "морозіла" in message.text.lower():
-            await message.reply("Чую починається обсос мол.")
-
+    if "морозіла" in message.text.lower():
+        await message.reply("Чую починається обсос мол.")
 
 async def main():
+    print("Бот запущений...")
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
