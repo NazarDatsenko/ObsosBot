d17866a91610d2b50d4717efa15fb1a386d0f469
NazarDatsenko <steamsupernazar@gmail.com>
2026-01-09 02:05:11 +0200

    changed command for hello

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ChatType
 from aiogram.types import FSInputFile
+from aiogram.filters import Command
 import os
 
 bot = Bot(token=os.environ["BOT_TOKEN"])
@@ -14,7 +15,7 @@ async def group_only(message: types.Message):
     if message.chat.type not in (ChatType.GROUP, ChatType.SUPERGROUP):
         return
 
-@dp.message(lambda m: m.chat.type in ("group", "supergroup") and m.text == "/hello@obsosAMbot")
+@dp.message(Command("hello"), lambda m: m.chat.type in ("group", "supergroup"))
 async def send_voice(message: types.Message):
     voice = FSInputFile("hello.ogg")
     await message.answer_voice(voice)
@@ -22,7 +23,7 @@ async def send_voice(message: types.Message):
 @dp.message()
 async def keyword(message: types.Message):
     if message.chat.type.name.endswith("GROUP"):
-        if "Морозіла" in message.text.lower():
+        if "морозіла" in message.text.lower():
             await message.reply("Чую починається обсос мол.")
 
 
